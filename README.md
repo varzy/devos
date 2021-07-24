a711b8684ef210e4f2ab1421386be2b22d202fb7
zy <varzy@live.com>
2021-07-24 16:28:39 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # DOCKER
+
+```bash
+# 停止全部容器
+docker stop $(docker ps -aq)
+
+# 删除全部容器
+docker rm $(docker ps -aq)
+```
--- mysql/_init.sh
+++ mysql/_init.sh
@@ -1,6 +1,7 @@
 docker run \
   --name lc_mysql \
   -d \
+  --restart=always \
   -p 3306:3306 \
   -e MYSQL_ROOT_PASSWORD="pswd" \
   -v /Users/zy/Developer/_Docker/mysql/my.cnf:/etc/mysql/my.cnf \
